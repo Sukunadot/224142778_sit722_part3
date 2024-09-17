1345d582756dfcf5768d476068797f7570eb92e8
Sukunadot <85873221+Sukunadot@users.noreply.github.com>
2024-09-17 16:10:11 +1000

    Add files via upload

---
+++ book_catalog/app/config.py
@@ -0,0 +1,3 @@
+import os
+
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://bhanu_sit722_part3_user:Rj6pTYQQU8eEIKd73Vh3p3sqjKTKemux@dpg-crdu26ggph6c73ek4980-a.oregon-postgres.render.com/bhanu_sit722_part3')
